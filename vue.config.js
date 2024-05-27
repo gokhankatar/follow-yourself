a54898d190aea87b1bd4f38aa13535bc95acf71e
gokhankatar <katar_gokhan@hotmail.com>
2024-05-27 17:18:44 +0300

    fixed server options

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-
   pluginOptions: {
     vuetify: {},
   },
+  devServer: {
+    port: 5000,
+    open: true
+  }
 });
