db2cc8e614c6d91e2aa8590b0d57a31dfc56447f
gokhankatar <katar_gokhan@hotmail.com>
2024-06-08 19:47:57 +0300

    updated scrollbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script>
 /*
-* @description : Follow your developments and activities with algorithms
-* @author : Gokhan Katar
-* @github : https://github.com/gokhankatar
-* @x : https://twitter.com/gokhan_crypto/
-* @instagram :  https://www.instagram.com/katargokhan96/
-*/
+ * @description : Follow your developments and activities with algorithms
+ * @author : Gokhan Katar
+ * @github : https://github.com/gokhankatar
+ * @x : https://twitter.com/gokhan_crypto/
+ * @instagram :  https://www.instagram.com/katargokhan96/
+ */
 
 import Navbar from "./components/Navbar";
 
@@ -23,3 +23,11 @@ export default {
     </v-main>
   </v-app>
 </template>
+
+<style>
+html {
+  scrollbar-width: thin;
+  scrollbar-color: #0a6056 #f1f1f1;
+  scroll-behavior: smooth;
+}
+</style>
